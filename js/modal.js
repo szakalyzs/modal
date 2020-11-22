8638e489c1e72c308aa17d791220acb5decbc716
unknown <szakalyzs@gmail.com>
2020-11-22 10:06:02 +0100

    All done!

--- js/modal.js
+++ js/modal.js
@@ -3,33 +3,29 @@
 (function modalOpen() {
     const headerBtn = document.querySelector('.header__btn');
     headerBtn.addEventListener('click', () => {
-        const cover = document.querySelector('.cover--hide');
+        const cover = document.querySelector('.cover--blur');
         cover.setAttribute('class', 'cover');
+        const modal = document.querySelector('.modal');
         modal.focus();
     });
 })();
 
 (function modalClose() {
-    const modalCloseBtn = document.querySelector('.modal__close');
-    modalCloseBtn.addEventListener('click', () => {
-        const cover = document.querySelector('.cover');
-        cover.setAttribute('class', 'cover--hide');
-    })
+    const modalHide = document.querySelectorAll('.modal__close, .modal__okey, .modal__cancel');
+    for (let i = 0; i < modalHide.length; i++) {
+        modalHide[i].addEventListener('click', () => {
+            const cover = document.querySelector('.cover');
+            cover.setAttribute('class', 'cover--blur');
+        })
+    }
 })();
 
 (function modalClose2() {
-    const cover = document.querySelector('.cover--hide');
+    const cover = document.querySelector('.cover--blur');
     cover.addEventListener('click', (event) => {
         const cover = document.querySelector('.cover');
         if (event.target == cover) {
-            cover.setAttribute('class', 'cover--hide');
+            cover.setAttribute('class', 'cover--blur');
         }
     })
 })();
-
-/* (window.onclick = function (event) {
-    const cover = document.querySelector('.cover');
-    if (event.target == cover) {
-        cover.style.display = "none";
-    }
-})(); */
